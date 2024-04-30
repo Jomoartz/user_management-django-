42cec3938231ae23581ef5fc51dc38abcf8fc2f6
jomoartz <johnjohananmogbolu@gmail.com>
2024-04-30 07:26:06 -0700

    connected user_auth.urls to frontend

--- usermanagement_webapp_project/usermanagement_webapp_project/urls.py
+++ usermanagement_webapp_project/usermanagement_webapp_project/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('',include(user_auth.urls)),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('user_auth.urls')),
+
 ]
